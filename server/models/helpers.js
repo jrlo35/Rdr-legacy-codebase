b0da6e56ef6e8c75b28e0955c6b50362694280b3
Jonathon Lo <jonathon.lo35@gmail.com>
2016-03-07 19:35:55 -0800

    '(fix) Add rebase'

--- server/models/helpers.js
+++ server/models/helpers.js
@@ -358,17 +358,11 @@ module.exports = {
   addMeetup: addMeetup,
   getMeetups: getMeetups,
   getMeetupDetails: getMeetupDetails,
-<<<<<<< bad6366be0e667c35e14e16fe2228a6986d8009a
+
 
   // getUsersMeetups: getUsersMeetups,
 
-=======
-<<<<<<< cfd0b94cdb8ec5fb397b5943f3d4d51f70341c4d
-  // getUsersMeetups: getUsersMeetups,
-=======
-  getUserMeetups: getUserMeetups,
->>>>>>> '(feat) Add join table'
->>>>>>> '(feat) Add join table'
+
   getBookDetails: getBookDetails
 
 
